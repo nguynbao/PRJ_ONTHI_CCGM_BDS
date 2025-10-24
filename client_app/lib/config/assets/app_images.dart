a15031ccf0acf04405956402960249fda95e07f8
Nguyen Ngoc Bao <128023210+nguynbao@users.noreply.github.com>
2025-10-24 17:19:21 +0700

    Update app_images.dart

--- client_app/lib/config/assets/app_images.dart
+++ client_app/lib/config/assets/app_images.dart
@@ -2,5 +2,5 @@ class AppImages {
   static const String format = '.jpg';
   static const String base = 'assets/images/';
   static const String demoAvt = '${base}anh3$format';
-  
+  static const String test = '${base}test$format';
 }
